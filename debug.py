524e43d8cb47f83fc787e91c361713454e0863e2
Terminal1337 <str1ng.exe1337@protonmail.com>
2023-11-11 22:33:09 +0530

    updated files

--- debug.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-with open('data.json','r') as f:
-    data = json.load(f)
-    
-    
-i = data["data"]["user"]["result"]['legacy']['entities']['url']['urls'][0]['display_url']
-print(i)
